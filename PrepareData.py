a8f8ff8613513b51c2d2fc694b65f31aff315853
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-01-18 14:03:07 +0530

    Create PrepareData.py

---
+++ PrepareData.py
@@ -0,0 +1,32 @@
+import pandas
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/graphing.py
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/doggy-boot-harness.csv
+!pip install statsmodels
+
+
+# Make a dictionary of data for boot sizes
+# and harness sizes in cm
+data = {
+    'boot_size' : [ 39, 38, 37, 39, 38, 35, 37, 36, 35, 40, 
+                    40, 36, 38, 39, 42, 42, 36, 36, 35, 41, 
+                    42, 38, 37, 35, 40, 36, 35, 39, 41, 37, 
+                    35, 41, 39, 41, 42, 42, 36, 37, 37, 39,
+                    42, 35, 36, 41, 41, 41, 39, 39, 35, 39
+ ],
+    'harness_size': [ 58, 58, 52, 58, 57, 52, 55, 53, 49, 54,
+                59, 56, 53, 58, 57, 58, 56, 51, 50, 59,
+                59, 59, 55, 50, 55, 52, 53, 54, 61, 56,
+                55, 60, 57, 56, 61, 58, 53, 57, 57, 55,
+                60, 51, 52, 56, 55, 57, 58, 57, 51, 59
+                ]
+}
+
+# Convert it into a table using pandas
+dataset = pandas.DataFrame(data)
+
+# Print the data
+# In normal python we would write
+# print(dataset)
+# but in Jupyter notebooks, we simply write the name
+# of the variable and it is printed nicely 
+dataset
