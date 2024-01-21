a8d2e0a71a2a7f85e61be372813976167252b81f
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-01-21 22:01:43 +0530

    Create ImageAnalysis.cs

---
+++ ImageAnalysis.cs
@@ -0,0 +1,19 @@
+using Azure.AI.Vision.Common;
+using Azure.AI.Vision.ImageAnalysis;
+
+var serviceOptions = new VisionServiceOptions(
+    Environment.GetEnvironmentVariable("VISION_ENDPOINT"),
+    new AzureKeyCredential(Environment.GetEnvironmentVariable("VISION_KEY")));
+
+using var imageSource = VisionSource.FromUrl(new Uri("<url>"));
+
+var options = new ImageAnalysisOptions()
+{
+    Features = ImageAnalysisFeature.Caption | ImageAnalysisFeature.Text,
+    Language = "en",
+    GenderNeutralCaption = true
+};
+
+using var analyzer = new ImageAnalyzer(serviceOptions, imageSource, options);
+
+var result = analyzer.Analyze();
