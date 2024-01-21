3d47c75db24ec5fb833d857141ad04fcb13f8c5d
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2024-01-21 17:38:17 +0530

    Create ClassificationMetrics.py

---
+++ ClassificationMetrics.py
@@ -0,0 +1,175 @@
+#LATERNATIVE METRICS FOR BINARY CLASSIFIERS
+import pandas as pd
+from matplotlib import pyplot as plt
+%matplotlib inline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+# load the training dataset
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/diabetes.csv
+diabetes = pd.read_csv('diabetes.csv')
+
+# Separate features and labels
+features = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']
+label = 'Diabetic'
+X, y = diabetes[features].values, diabetes[label].values
+
+
+# Split data 70%-30% into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+print ('Training cases: %d\nTest cases: %d' % (X_train.shape[0], X_test.shape[0]))
+
+# Train the model
+from sklearn.linear_model import LogisticRegression
+
+# Set regularization rate
+reg = 0.01
+
+# train a logistic regression model on the training set
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+print('Predicted labels: ', predictions)
+print('Actual labels:    ', y_test)
+
+
+print('Accuracy: ', accuracy_score(y_test, predictions))
+
+
+
+#ALTERNATE WAY TO ACCESS MODEL 
+from sklearn. metrics import classification_report
+
+print(classification_report(y_test, predictions))
+
+
+#ROC_AUC_SCORE
+from sklearn.metrics import roc_auc_score
+
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('AUC: ' + str(auc))
+
+
+
+# Train the model
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+
+# Define preprocessing for numeric columns (normalize them so they're on the same scale)
+numeric_features = [0,1,2,3,4,5,6]
+numeric_transformer = Pipeline(steps=[
+    ('scaler', StandardScaler())])
+
+# Define preprocessing for categorical features (encode the Age column)
+categorical_features = [7]
+categorical_transformer = Pipeline(steps=[
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+# Combine preprocessing steps
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+# Create preprocessing and training pipeline
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('logregressor', LogisticRegression(C=1/reg, solver="liblinear"))])
+
+
+# fit the pipeline to train a logistic regression model on the training set
+model = pipeline.fit(X_train, (y_train))
+print (model)
+
+# Get predictions from test data
+predictions = model.predict(X_test)
+y_scores = model.predict_proba(X_test)
+
+# Get evaluation metrics
+cm = confusion_matrix(y_test, predictions)
+print ('Confusion Matrix:\n',cm, '\n')
+print('Accuracy:', accuracy_score(y_test, predictions))
+print("Overall Precision:", precision_score(y_test, predictions))
+print("Overall Recall:", recall_score(y_test, predictions))
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('AUC: ' + str(auc))
+
+# calculate ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_scores[:,1])
+
+# plot ROC curve
+fig = plt.figure(figsize=(6, 6))
+# Plot the diagonal 50% line
+plt.plot([0, 1], [0, 1], 'k--')
+# Plot the FPR and TPR achieved by our model
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
+
+#ESEMBLE ALGORITHM USING RANDOM FOREST
+from sklearn.ensemble import RandomForestClassifier
+
+# Create preprocessing and training pipeline
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('logregressor', RandomForestClassifier(n_estimators=100))])
+
+# fit the pipeline to train a random forest model on the training set
+model = pipeline.fit(X_train, (y_train))
+print (model)
+
+predictions = model.predict(X_test)
+y_scores = model.predict_proba(X_test)
+cm = confusion_matrix(y_test, predictions)
+print ('Confusion Matrix:\n',cm, '\n')
+print('Accuracy:', accuracy_score(y_test, predictions))
+print("Overall Precision:",precision_score(y_test, predictions))
+print("Overall Recall:",recall_score(y_test, predictions))
+auc = roc_auc_score(y_test,y_scores[:,1])
+print('\nAUC: ' + str(auc))
+
+# calculate ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_scores[:,1])
+
+# plot ROC curve
+fig = plt.figure(figsize=(6, 6))
+# Plot the diagonal 50% line
+plt.plot([0, 1], [0, 1], 'k--')
+# Plot the FPR and TPR achieved by our model
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.show()
+
+
+#USE MODEL FOR INTERFCAING
+import joblib
+
+# Save the model as a pickle file
+filename = './diabetes_model.pkl'
+joblib.dump(model, filename)
+
+
+
+# Load the model from the file
+model = joblib.load(filename)
+
+# predict on a new sample
+# The model accepts an array of feature arrays (so you can predict the classes of multiple patients in a single call)
+# We'll create an array with a single array of features, representing one patient
+X_new = np.array([[2,180,74,24,21,23.9091702,1.488172308,22]])
+print ('New sample: {}'.format(list(X_new[0])))
+
+# Get a prediction
+pred = model.predict(X_new)
+
+# The model returns an array of predictions - one for each set of features submitted
+# In our case, we only submitted one patient, so our prediction is the first one in the resulting array.
+print('Predicted class is {}'.format(pred[0]))
